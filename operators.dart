3064c396daf907672600d8345a2cf11b6d7557f4
Ankita Bharat Badhe <113541646+ankitabadhe@users.noreply.github.com>
2022-09-14 22:27:21 +0530

    Add files via upload

---
+++ operators.dart
@@ -0,0 +1,80 @@
+void main() {
+  //Operators
+
+  //#Arithmetic Operators
+
+  // var a = 5;
+  // var b = 20;
+  // var c = 50;
+  // var d = 4;
+
+  // print(a + b);
+  // print(c - d);
+  // print(a * b);
+  // print(c / d);
+  // print(c ~/ d);
+  // print(c % d);
+
+  //#Prefix and Postfix Increment and Decrement operators
+
+  // var i = 5;
+  // print(i);
+  // print(++i);
+  // print(i);
+  // print(i++);
+  // print(i);
+  // print(--i);
+  // print(i);
+  // print(i--);
+  // print(i);
+
+  //#Equality and Relational operators
+
+  // var a = 5;
+  // var b = 6;
+  // print(a == b);
+  // print(a != b);
+  // print(a > b);
+  // print(a < b);
+  // print(a >= b);
+  // print(a <= b);
+
+  //#Logical Operators
+
+  /*
+      true && true = true
+      true && false = false
+      false && true = false
+      false && false = false
+
+      true || true = true
+      true || false = true
+      false || true = true
+      false || false = false
+  */
+
+  // var a = 600;
+  // var b = 600;
+  // var c = 200;
+  // var d = 200;
+  // print((a == b) && (c == d));
+  // print((a == d) || (c == b));
+
+  //#Type Test Operators
+
+  // var name = "Abhay";
+  // var number = 100;
+  // print(name is String);
+  // print(name is! String);
+  // print(number is int);
+  // print(number is! int);
+
+//   //#Assignment Operators
+
+  var n = 5;
+
+  // n += 5; // n = n + 5
+  // n -= 5; // n = n - 5
+  n *= 5; // n = n * 5
+  print(n);
+}
---
+++ variables.dart
@@ -0,0 +1,21 @@
+void main() {
+  //Variable Name and Rules
+  // # Valid Variable Name
+
+  var f_name = "Aditya";
+  var l_name = "Rane";
+  var place = "Pune";
+  var phone_no = 9881234578;
+
+  // # Invalid Variable Name
+
+  //var 11Robot= "Chitti";
+  //var for = "Gun";
+
+  //Printing Variables
+
+  print(f_name);
+  print(l_name);
+  print(place);
+  print(phone_no);
+}

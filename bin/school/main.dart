d20f1d77ceb8922b2319fb6302fe37ad2c5ddc51
Ankita Bharat Badhe <113541646+ankitabadhe@users.noreply.github.com>
2022-09-15 11:27:22 +0530

    Create main.dart

---
+++ bin/school/main.dart
@@ -0,0 +1,25 @@
+// import 'vars.dart' show Ankita, display1;
+
+// void main() {
+//   print(Ankita);
+//   display1();
+//   // print(Aditya);
+//   // display2();
+//   // print(Pranjal);
+//   // display3();
+//   // print(Krishna);
+//   // display4();
+// }
+
+import 'vars.dart' hide Ankita, display1;
+
+void main() {
+  // print(Ankita);
+  // display1();
+  print(Aditya);
+  display2();
+  print(Pranjal);
+  display3();
+  print(Krishna);
+  display4();
+}

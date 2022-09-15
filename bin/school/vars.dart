f3f8d9e378b48ff2e98f81d85dcc9b69202e5155
Ankita Bharat Badhe <113541646+ankitabadhe@users.noreply.github.com>
2022-09-15 11:27:39 +0530

    Add files via upload

---
+++ bin/school/vars.dart
@@ -0,0 +1,20 @@
+var Ankita = 100;
+var Aditya = 200;
+var Pranjal = 300;
+var Krishna = 400;
+
+display1() {
+  print('Display 1');
+}
+
+display2() {
+  print('Display 2');
+}
+
+display3() {
+  print('Display 3');
+}
+
+display4() {
+  print('Display 4');
+}
